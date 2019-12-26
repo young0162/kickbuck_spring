be0c248105c25004c27eb42d55c28c17c9134897
jdy0162 <wkdehdud3@naver.com>
2019-12-26 17:17:00 +0900

    Auto stash before merge of "master" and "origin/master"

---
+++ src/main/java/com/spring/controller/BucketController.java
@@ -0,0 +1,28 @@
+package com.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.service.GuestService;
+
+@Controller
+public class BucketController {
+
+	@Autowired
+	private GuestService service;
+	
+	@RequestMapping("/guest/list")
+	public ModelAndView list()
+	{
+		ModelAndView model = new ModelAndView();
+		int totalCount = service.getTotalCount();
+		
+		model.addObject("totalCount",totalCount);
+		model.setViewName("/macro/guest/guestlist"); //tiles.xml 에서 지정한 이름으로 layout
+		
+		return model; 	
+	}
+	
+}
---
+++ src/main/java/com/spring/service/BucketService.java
@@ -0,0 +1,19 @@
+package com.spring.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.spring.dao.GuestDao;
+
+@Service
+public class BucketService {
+	
+	@Autowired
+	private GuestDao dao;
+	
+	public int getTotalCount()
+	{
+		return dao.getTotalCount(); 
+	}
+	
+}
