250dab2857a1deb9b7c6fae691de7ca7410272dd
jdy0162 <wkdehdud3@naver.com>
2019-12-27 18:12:44 +0900

    doyoung 12 27 버킷리스트 등록
    doyoung 12 27 버킷리스트 등록

--- src/main/java/com/spring/dao/BucketDao.java
+++ src/main/java/com/spring/dao/BucketDao.java
@@ -1,14 +1,33 @@
 package com.spring.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.support.SqlSessionDaoSupport;
 import org.springframework.stereotype.Repository;
+
+import com.spring.dto.BucketDto;
 
 @Repository
 public class BucketDao extends SqlSessionDaoSupport{
 	
-	public int getTotalCount()
+	public void insertBucket(BucketDto dto)
 	{
-		return getSqlSession().selectOne("guestTotalcount");
+		getSqlSession().insert("bucketInsert",dto);
+	}
+	
+	public void bucketWithUp(int num)
+	{
+		getSqlSession().update("bucketWithUp",num);
+	}
+	
+	public void bucketLikeUp(int num)
+	{
+		getSqlSession().update("bucketLikeUp",num);
+	}
+	
+	public List<BucketDto> AllSelect()
+	{
+		return getSqlSession().selectList("AllSelect");
 	}
 	
 }
--- src/main/java/com/spring/service/BucketService.java
+++ src/main/java/com/spring/service/BucketService.java
@@ -1,11 +1,38 @@
 package com.spring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.spring.dao.BucketDao;
+import com.spring.dto.BucketDto;
 
 
 @Service
 public class BucketService {
 	
+	@Autowired
+	private BucketDao dao;
+	
+	public void insertBucket(BucketDto dto)
+	{
+		dao.insertBucket(dto);
+	}
+	
+	public void bucketWithUp(int num)
+	{
+		dao.bucketWithUp(num);
+	}
+	
+	public void bucketLiekUp(int num)
+	{
+		dao.bucketLikeUp(num);
+	}
+	
+	public List<BucketDto> AllSelect()
+	{
+		return dao.AllSelect();
+	}
 	
 }
